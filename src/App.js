966dc49e694279835aca06efde7517ced090a178
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-05-21 19:14:38 +1000

    cominicate items and build and style accordion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React from "react";
+import Accordion from "./components/Accordion";
 
-export default ()=>{
-    return <h1>Widget App</h1>;
-};
+  const items = [
+    {
+      title: "What is React?",
+      content: "React is a front end javascript framework",
+    },
+    {
+      title: "Why use React?",
+      content: "React is a favorite JS library among engineers",
+    },
+    {
+      title: "How do you use React?",
+      content: "You use React by creating components",
+    },
+  ];
+  const App = () => {
+   return (
+     <div>
+       <Accordion items={items}/>
+     </div>
+   );
+  };
+  export default App;
