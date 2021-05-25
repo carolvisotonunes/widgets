54cac17f01d10eb5f4c44a0c9fdbbb6d42aa0423
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-05-25 16:57:04 +1000

    implement delay request

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,23 +6,27 @@ const Search = () => {
     const[results, setResults] = useState([]);
     console.log(results);
     useEffect( ()=>{
-      const search = async ()=>{
-        const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
-            params:{
-              action:'query',
-              list:'search',
-              origin:'*',
-              format:'json',
-              srsearch:term,
-            },
-        });
-        setResults(data.query.search);
-      };
-     const timeOut = setTimeout(()=>{
-        if(term){
-          search();
-        }
-      }, 500);
+          const search = async ()=>{
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
+                params:{
+                  action:'query',
+                  list:'search',
+                  origin:'*',
+                  format:'json',
+                  srsearch:term,
+                },
+            });
+            setResults(data.query.search);
+          };
+        const timeoutId = setTimeout(() => {
+           if (term) {
+             search();
+           }
+        }, 1000);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
     }, [term] );
 
     const renderedResults= results.map((result)=>{
