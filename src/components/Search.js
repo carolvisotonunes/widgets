c603695d2a81bc18de1716713e75a248041b0913
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-05-26 17:14:02 +1000

    option video fix warning

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,78 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-    const[term, setTerm] = useState('programming');
-    const[results, setResults] = useState([]);
-    console.log(results);
-    useEffect( ()=>{
-          const search = async ()=>{
-            const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
-                params:{
-                  action:'query',
-                  list:'search',
-                  origin:'*',
-                  format:'json',
-                  srsearch:term,
-                },
-            });
-            setResults(data.query.search);
-          };
+  const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  const [results, setResults] = useState([]);
 
-        if(term && !results.length){
-           search();
-        }else{
-            const timeoutId = setTimeout(() => {
-                 if (term) {
-                   search();
-                 }
-            }, 1000);
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
 
-            return () => {
-              clearTimeout(timeoutId);
-            };
-        }
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
-    }, [term] );
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: debouncedTerm,
+        },
+      });
 
-    const renderedResults= results.map((result)=>{
-      return(
-        <div key ={result.pageid} className="item">
-          <div className="right floated content">
-             <a
-               className="ui button"
-               href={`https://en.wikipedia.org?curid=${result.pageid}`}
-             >
-                Go
-             </a>
-          </div>
-          <div className="content">
-            <div className="header">
-                {result.title}
-            </div>
-                <span dangerouslySetInnerHTML={{__html:result.snippet}}></span>
-          </div>
+      setResults(data.query.search);
+    };
+    if (debouncedTerm) {
+      search();
+    }
+  }, [debouncedTerm]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
         </div>
-      );
-    });
-
-    return(
-        <div>
-            <div className= "ui form">
-                <div className="field">
-                    <label>Enter the Term</label>
-                    <input
-                      value={term}
-                      onChange={(e) => setTerm(e.target.value)}
-                      className="input"
-                    />
-                </div>
-            </div>
-            <div className ="ui celled list">
-                {renderedResults}
-            </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
+      </div>
     );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
 };
 
 export default Search;
