26b91a2197ce73b6804166a21a9e8529f233f36a
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-05-28 23:29:41 +1000

    handling command clicks

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    if(event.metaKey || event.ctrlKey){
+        return;
+    }
     event.preventDefault();//prevent to reload  the full page
     window.history.pushState({},'',href);
     const navEvent = new PopStateEvent('popstate');
