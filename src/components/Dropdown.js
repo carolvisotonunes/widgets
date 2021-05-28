998d587fe53e232c7d91cc5e86606091d2e07dbf
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-05-28 18:23:26 +1000

    add language input

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ label, options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
   const ref = useRef();
 
@@ -18,16 +18,6 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         capture: true,
       });
     };
-  }, []);
-
-  useEffect(() => {
-    document.body.addEventListener(
-      "click",
-      () => {
-        setOpen(false);
-      },
-      { capture: true }
-    );
   }, []);
 
   const renderedOptions = options.map((option) => {
@@ -49,7 +39,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   return (
     <div ref={ref} className="ui form">
       <div className="field">
-        <label className="label">Select a Color</label>
+        <label className="label">{label}</label>
         <div
           onClick={() => setOpen(!open)}
           className={`ui selection dropdown ${open ? "visible active" : ""}`}
