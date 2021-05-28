8a4d4c1117a5ddfae768b42759ad0f0647ff1021
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-05-28 23:09:51 +1000

    detecting navigation

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
-    event.preventDefault();//prevent to reload all the page content
+    event.preventDefault();//prevent to reload  the full page
+    window.history.pushState({},'',href);
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);//communicate over to those root components that URl has just changed
   };
 
   return (
